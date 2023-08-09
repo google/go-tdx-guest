050bc72c84c5a47b0ef882e7f2238d35fb5bd420
Sandeep Panwar <panwarsandeep@gmail.com>
2023-08-09 16:43:54 +0000

    Revert "Fixing GET REPORT ioctl"
    This reverts commit 8c332389aed9308cf94a031837fabf55584b9f4d.

--- client/linuxabi/linux_abi.go
+++ client/linuxabi/linux_abi.go
@@ -34,12 +34,15 @@ const (
 	iocRead      = 2
 	// Linux /dev/tdx-guest ioctl interface
 	iocTypeTdxGuestReq = 'T'
-	iocTdxWithoutNrWithoutSize    = ((iocWrite | iocRead) << iocDirshift) |
-		(iocTypeTdxGuestReq << iocTypeshift)
-	// IocTdxGetReport is the ioctl command for getting an attestation report.
-	IocTdxGetReport = iocTdxWithoutNrWithoutSize | (unsafe.Sizeof(TdxReportReq{}) << iocSizeshift) | (0x1 << iocNrshift)
-	// IocTdxGetQuote is the ioctl command for getting an attestation quote.
-	IocTdxGetQuote = iocTdxWithoutNrWithoutSize | (unsafe.Sizeof(TdxQuoteReqABI{}) << iocSizeshift) | (0x2 << iocNrshift)
+	iocTdxWithoutNr    = ((iocWrite | iocRead) << iocDirshift) |
+		(iocTypeTdxGuestReq << iocTypeshift) |
+		(64 << iocSizeshift)
+	// IocTdxGetReport is the ioctl command for getting an attestation report
+	IocTdxGetReport = iocTdxWithoutNr | (0x1 << iocNrshift)
+	// IocTdxGetQuote is the ioctl command for getting an attestation quote
+	IocTdxGetQuote = ((iocRead) << iocDirshift) |
+		(iocTypeTdxGuestReq << iocTypeshift) | (0x2 << iocNrshift) |
+		(64 << iocSizeshift)
 	// TdReportDataSize is a constant for TDX ReportData size
 	TdReportDataSize = 64
 	// TdReportSize is a constant for TDX Report size
